54548f62be86b24e0ac9617db3810e2ab2698e10
DaveMitten <davidmitten88@gmail.com>
2020-10-20 14:19:58 +0100

    grid now works with dnymaic creation

--- src/components/articleList.js
+++ src/components/articleList.js
@@ -6,16 +6,16 @@ import React from "react"
 
 
 export default ({props}) => (
-    <div className="flex h-64 ">
-        <div className="flex-none h-64 w-16 bg-white mx-16 pt-48">
+    <div className="flex h-64 justify-center">
+        <div className="flex-none h-64 w-12 bg-white mx-16 pt-48">
             <div className="transform -rotate-90 align-baseline text-gray-500">Articles...</div>
         </div>
-        <div className='flex-1 grid grid-flow-col overflow-auto'>
+        <div className='flex-1 grid grid-cols-3 auto-cols-auto overflow-auto max-w-4xl gap-4 min-h-full'>
             {props.allContentfulBlogPost.edges.map(({node}) => {
                 return (
-                    <div className="h-64 w-48" key={node.slug}>
+                    <div className="h-64 w-48 mb-3 bg-white" key={node.slug}>
                         <img className="h-32 w-48" src={node.heroImage.file.url} alt=""/>
-                        <div className="bg-white p-2 h-full">
+                        <div className="p-2">
                             <div>{node.title}</div>
                             <div>{node.tags}</div>
                         </div>
@@ -24,7 +24,7 @@ export default ({props}) => (
             })}
         </div>
         <div className="flex-none h-64 w-12 bg-white mx-16 pt-10">
-            <div className="transform rotate-90 text-gray-500">More...</div>
+            <div className="transform rotate-90 text-gray-500">More >>></div>
         </div>
     </div>
 )
